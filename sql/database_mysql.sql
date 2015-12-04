40c9613fb5e8d42f85604f248768d5bda224178a
Mateusz Brycki <mateusz.brycki@outlook.com>
2015-12-04 20:32:28 +0100

    DB-1 - column types and default values - conflicts with hibernate, drop sequence

--- sql/database_mysql.sql
+++ sql/database_mysql.sql
@@ -13,25 +13,29 @@ CREATE TABLE user_account (
 	password VARCHAR(255) NOT NULL,
 	is_active BOOLEAN DEFAULT TRUE,
 	audit_cd TIMESTAMP NOT NULL,
-	audit_md TIMESTAMP,
+	audit_md DATE DEFAULT NULL,
 	FOREIGN KEY (fk_role_id)
 		REFERENCES user_role (role_id)
 		ON UPDATE CASCADE
 );
 CREATE TABLE message (
 	message_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	fk_author_id INTEGER NOT NULL,
 	topic TEXT NOT NULL,
 	text TEXT NOT NULL,
 	audit_cd TIMESTAMP NOT NULL,
-	audit_md TIMESTAMP
+	audit_md DATE DEFAULT NULL,
+	FOREIGN KEY (fk_author_id)
+		REFERENCES user_account(user_id)
+		ON UPDATE CASCADE
 );
 CREATE TABLE message_receiver (
 	fk_message_id INTEGER,
 	fk_user_id INTEGER,
-	is_read boolean NOT NULL,
-	read_date TIMESTAMP,
+	is_read boolean NOT NULL DEFAULT false,
+	read_date DATE DEFAULT NULL,
 	audit_cd TIMESTAMP NOT NULL,
-	autid_md TIMESTAMP NOT NULL,
+	autid_md DATE DEFAULT NULL,
 	PRIMARY KEY(fk_message_id, fk_user_id),
 	FOREIGN KEY (fk_message_id)
 		REFERENCES message (message_id)
@@ -47,7 +51,7 @@ CREATE TABLE folder (
 	label_color INTEGER,
 	owner_id INTEGER NOT NULL,
 	audit_cd TIMESTAMP NOT NULL,
-	audit_md TIMESTAMP
+	audit_md DATE DEFAULT NULL
 );
 CREATE TABLE user_message_folder (
 	fk_message_id INTEGER,
@@ -70,7 +74,7 @@ CREATE TABLE contact_book (
 	fk_owner_id INTEGER,
 	description VARCHAR(255),
 	audit_cd TIMESTAMP,
-	audit_md TIMESTAMP,
+	audit_md DATE DEFAULT NULL,
 	FOREIGN KEY (fk_owner_id)
 		REFERENCES user_account (user_id)
 		ON UPDATE CASCADE
@@ -93,12 +97,19 @@ CREATE TABLE user_contacts (
 
 );
 
-
 --DATA
-INSERT INTO `user_role` (`id`, `role`) VALUES
+INSERT INTO `user_role` (`id`, `role_id`) VALUES
 (1, 'ROLE_ADMIN'),
 (2, 'ROLE_MODERATOR'),
 (3, 'ROLE_USER');
 
+--DROP TABLE user_contacts;
+--DROP TABLE contact_book;
+--DROP TABLE user_message_folder;
+--DROP TABLE folder;
+--DROP TABLE message_receiver;
+--DROP TABLE message;
+--DROP TABLE user_account;
+--DROP TABLE user_role;
 
 
